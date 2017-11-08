e759f089730337d78c8d0de6119b924ff3a73b67
Martin Bierbaum <martin.bierbaum@rub.de>
2017-11-08 10:41:56 +0100

    get version from scm

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-tex',
-    version='0.2',
+    # version='0.2',
     description='A simple Django app to render Latex templates and compile them into Pdf files.',
     url='https://github.com/weinbusch/django-tex',
     author='Martin Bierbaum',
     license='MIT',
     keywords='django latex jinja2',
     packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=[
+        'setuptools_scm',
+        ],
     install_requires=[
         'django>=1.11.4',
         'jinja2>=2.9.6',
