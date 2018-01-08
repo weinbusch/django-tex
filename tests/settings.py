b05387e584e9d8219cd0bfc711e7e41dd46d7dc7
Martin Bierbaum <martin.bierbaum@rub.de>
2018-01-08 18:10:22 +0100

    Add internationalization settings; tests fail!

--- tests/settings.py
+++ tests/settings.py
@@ -28,9 +28,15 @@ TEMPLATES = [
     },
 ]
 
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = 'de-de'
+
+TIME_ZONE = 'Europe/Berlin'
+
+USE_I18N = True
 
 USE_L10N = True
+
+USE_TZ = True
 
 ROOT_URLCONF = 'tests.urls'
 
