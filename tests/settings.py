0e132ebb15031bb5c6bf661dfc257eb4e1f27ec1
Martin Bierbaum <martin.bierbaum@rub.de>
2018-10-22 15:49:36 +0200

    Remove TEMPLATES from test settings, as it isn't needed

--- tests/settings.py
+++ tests/settings.py
@@ -20,14 +20,6 @@ DATABASES = {
     }
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-    },
-]
-
 LANGUAGE_CODE = 'de-de'
 
 TIME_ZONE = 'Europe/Berlin'
