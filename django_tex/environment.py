f68b4dbe6e7934f259ed3244d9504be3990659b1
Martin Bierbaum <martin.bierbaum@rub.de>
2019-02-04 21:15:07 +0100

    Do not overwrite built-in django filters
    By using register.filters.copy() in environment.py, unintended
    overwriting of the built-in linebreaks filters is prevented.

--- django_tex/environment.py
+++ django_tex/environment.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import register
 from django_tex.filters import FILTERS as tex_specific_filters
 
 # Django's built-in filters ...
-filters = register.filters
+filters = register.filters.copy()
 # ... updated with tex specific filters
 filters.update(tex_specific_filters)
 
