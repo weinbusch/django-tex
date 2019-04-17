0ac5292cfbadc073d806cc66bde1932e1fa298d3
Martin Bierbaum <martin.bierbaum@rub.de>
2019-04-17 14:17:19 +0200

    display deprecationwarning only if render_to_pdf is called

--- django_tex/views.py
+++ django_tex/views.py
@@ -1,9 +1,11 @@
 import warnings
 
-from django_tex.shortcuts import render_to_pdf
-warnings.warn(
-    ('"from django_tex.views import render_to_pdf" is deprectated '
-    'and will be removed in future releases. Please use '
-    '"from django_tex.shortcuts import render_to_pdf" instead.'),
-    PendingDeprecationWarning
-) 
+def render_to_pdf(*args, **kwargs):
+    from django_tex.shortcuts import render_to_pdf as func
+    warnings.warn(
+        ('"django_tex.views.render_to_pdf" is deprecated '
+        'and will be removed in future releases. Please use '
+        '"django_tex.shortcuts.render_to_pdf" instead.'),
+        PendingDeprecationWarning
+    ) 
+    return func(*args, **kwargs)
