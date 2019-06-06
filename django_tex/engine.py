d5010aedf9f66372e17992fa0c5f66e6abb61547
Martin Bierbaum <martin.bierbaum@rub.de>
2019-06-06 20:27:24 +0200

    refactor engine

--- django_tex/engine.py
+++ django_tex/engine.py
@@ -1,11 +1,11 @@
 from django.template.backends.jinja2 import Jinja2
 
+DEFAULT_ENVIRONMENT = 'django_tex.environment.environment'
 
 class TeXEngine(Jinja2):
     app_dirname = 'templates'
 
     def __init__(self, params):
-        default_environment = 'django_tex.environment.environment'
         if 'environment' not in params['OPTIONS'] or not params['OPTIONS']['environment']:
-            params['OPTIONS']['environment'] = default_environment
+            params['OPTIONS']['environment'] = DEFAULT_ENVIRONMENT
         super().__init__(params)
