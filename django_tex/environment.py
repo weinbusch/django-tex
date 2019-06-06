758f79b045284e3e8a0670ca6db1812f34305af0
Martin Bierbaum <martin.bierbaum@rub.de>
2019-06-06 20:26:44 +0200

    refactor environment

--- django_tex/environment.py
+++ django_tex/environment.py
@@ -3,6 +3,9 @@ from jinja2 import Environment
 from django_tex.filters import FILTERS
 
 def environment(**options):
-    env = Environment(**options, extensions=['django_tex.extensions.GraphicspathExtension'])
+    options.update(
+        {'extensions': ['django_tex.extensions.GraphicspathExtension']}
+    )
+    env = Environment(**options)
     env.filters = FILTERS
     return env
