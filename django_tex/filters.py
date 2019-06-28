f48ebf8a718cd3f81e04f8a23f20e243ed0829b4
Martin Bierbaum <martin.bierbaum@rub.de>
2019-06-28 15:11:45 +0200

    filter for escaping latex special characters

--- django_tex/filters.py
+++ django_tex/filters.py
@@ -4,9 +4,21 @@ from django.template.defaultfilters import register
 def do_linebreaks(value):
     return value.replace('\n', '\\\\\n')
 
+def do_latex_escape(value):
+    return (value
+        .replace('&', '\\&')
+        .replace('$', '\\$')
+        .replace('%', '\\%')
+        .replace('#', '\\#')
+        .replace('_', '\\_')
+        .replace('{', '\\{')
+        .replace('}', '\\}')
+        )
+
 tex_specific_filters = {
     'localize': localize_input,
-    'linebreaks': do_linebreaks
+    'linebreaks': do_linebreaks,
+    'latex_escape': do_latex_escape,
 }
 
 FILTERS = register.filters.copy()
