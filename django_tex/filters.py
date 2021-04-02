a90a4f58e686b1b7ea22c50b0638f105388b3db3
Carli* Freudenberg <kound@posteo.de>
2021-04-02 16:46:40 +0200

    make sure everything is escaped correctly

--- django_tex/filters.py
+++ django_tex/filters.py
@@ -1,20 +1,43 @@
 from django.utils.formats import localize_input
 from django.template.defaultfilters import register
 
+BACKSLASH_REPLACEMENT = "<:-!~#BACKSLASH#~!:->"
 
-def do_linebreaks(value):
+
+def do_linebreaks(value: str) -> str:
     return value.replace("\n", "\\\\\n")
 
 
-def do_latex_escape(value):
+def do_latex_escape(value: str) -> str:
+    """
+    Replace all LaTeX characters that could cause the latex compiler to fail
+    and at the same time try to display the character as intended from the user.
+
+    see also https://tex.stackexchange.com/questions/34580/escape-character-in-latex
+    """
+
     return (
-        value.replace("&", "\\&")
-        .replace("$", "\\$")
-        .replace("%", "\\%")
-        .replace("#", "\\#")
-        .replace("_", "\\_")
-        .replace("{", "\\{")
-        .replace("}", "\\}")
+        # Replace Backslashes so we dont' double replace backslashes
+        value.replace("\\", BACKSLASH_REPLACEMENT)
+        # If there was a space before the character we assume the user wants
+        # it also in the displayed LaTeX text
+        .replace(BACKSLASH_REPLACEMENT + " ", r"\textbackslash\space ")
+        # Otherwise we assume it doesn't matter. Note the space at the end is
+        # required that an additional text is not considered as part of the latex
+        # command
+        .replace(BACKSLASH_REPLACEMENT, r"\textbackslash ")
+        # Do the same for ~ and ^
+        .replace("~ ", r"\textasciitilde\space ")
+        .replace("~", r"\textasciitilde ")
+        .replace("^ ", r"\textasciicircum\space ")
+        .replace("^", r"\textasciicircum ")
+        .replace("&", r"\&")
+        .replace("$", r"\$")
+        .replace("%", r"\%")
+        .replace("#", r"\#")
+        .replace("_", r"\_")
+        .replace("{", r"\{")
+        .replace("}", r"\}")
     )
 
 
