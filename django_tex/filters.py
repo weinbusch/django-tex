104163ec1dfa25703effee443b8f48d014ff772b
Martin Bierbaum <martin.bierbaum@rub.de>
2021-05-18 20:50:17 +0200

    refactor filters.py
    - filters.py now has a mapping, REPLACEMENTS, of strings to be
      replaced and their respective replacement
    
    - TODO: the vertical tab is not replaced and throws an error in the
      tests.

--- django_tex/filters.py
+++ django_tex/filters.py
@@ -1,11 +1,29 @@
+import re
+
 from django.utils.formats import localize_input
 from django.template.defaultfilters import register
-
-BACKSLASH_REPLACEMENT = "<:-!~#BACKSLASH#~!:->"
 
 
 def do_linebreaks(value: str) -> str:
     return value.replace("\n", "\\\\\n")
+
+
+REPLACEMENTS = dict(
+    [
+        ("&", "\\&"),
+        ("%", "\\%"),
+        ("$", "\\$"),
+        ("#", "\\#"),
+        ("_", "\\_"),
+        ("{", "\\{"),
+        ("}", "\\}"),
+        ("\\", "\\textbackslash{}"),
+        ("~", "\\textasciitilde{}"),
+        ("^", "\\textasciicircum{}"),
+    ]
+)
+
+ESCAPE_PATTERN = re.compile("[{}]".format("".join(map(re.escape, REPLACEMENTS.keys()))))
 
 
 def do_latex_escape(value: str) -> str:
@@ -15,33 +33,7 @@ def do_latex_escape(value: str) -> str:
 
     see also https://tex.stackexchange.com/questions/34580/escape-character-in-latex
     """
-
-    return (
-        # Replace Backslashes so we dont' double replace backslashes
-        value.replace("\\", BACKSLASH_REPLACEMENT)
-        # If there was a space before the character we assume the user wants
-        # it also in the displayed LaTeX text
-        .replace(BACKSLASH_REPLACEMENT + " ", r"\textbackslash\space ")
-        # Otherwise we assume it doesn't matter. Note the space at the end is
-        # required that an additional text is not considered as part of the latex
-        # command
-        .replace(BACKSLASH_REPLACEMENT, r"\textbackslash ")
-        # Do the same for ~ and ^
-        .replace("~ ", r"\textasciitilde\space ")
-        .replace("~", r"\textasciitilde ")
-        .replace("^ ", r"\textasciicircum\space ")
-        .replace("^", r"\textasciicircum ")
-        .replace("&", r"\&")
-        .replace("$", r"\$")
-        .replace("%", r"\%")
-        .replace("#", r"\#")
-        .replace("_", r"\_")
-        .replace("{", r"\{")
-        .replace("}", r"\}")
-        # Replace vertical tab as this seems to be the only character from
-        # string.printable that can't be consumed from LaTeX
-        .replace(chr(11), "")
-    )
+    return ESCAPE_PATTERN.sub(lambda mo: REPLACEMENTS.get(mo.group()), value)
 
 
 tex_specific_filters = {
