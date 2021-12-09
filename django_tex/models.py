478fb23c0f23ae69dfd0120577654179f71e0e98
grrodre <grrodre@gmail.com>
2021-12-09 20:19:59 +0000

    Replace ugettext_laxy
    Deprecated in Django 4 https://docs.djangoproject.com/en/dev/internals/deprecation/#deprecation-removed-in-4-0

--- django_tex/models.py
+++ django_tex/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.template import TemplateDoesNotExist
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.template.loader import get_template
 
 
