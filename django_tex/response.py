2ef9c569d005aaa670c105ef7ce3d836974ac093
gutmandev <Gutman.Josh@gmail.com>
2022-04-13 01:39:07 -0700

    Add as_attachment kwarg to PDFResponse and render_to_pdf

--- django_tex/response.py
+++ django_tex/response.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 
 
 class PDFResponse(HttpResponse):
-    def __init__(self, content, filename=None):
+    def __init__(self, content, as_attachment=False, filename=None):
         super(PDFResponse, self).__init__(content_type="application/pdf")
-        self["Content-Disposition"] = 'filename="{}"'.format(filename)
+        if filename:
+            disposition = "attachment" if as_attachment else "inline"
+            file_expr = 'filename="{}"'.format(filename)
+            self["Content-Disposition"] = "{}; {}".format(disposition, file_expr)
+        elif as_attachment:
+            self["Content-Disposition"] = "attachment"
         self.write(content)
